9e1d6d35bd30be22e2efd3a080dff3813a2b48ad
Matthew Sitton <matthewsitton@gmail.com>
2023-02-04 01:55:12 -0600

    Basic camera follow script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cam;
+
+    [SerializeField]
+    private Transform playerTransform;
+
+    void Start()
+    {
+        // set to player position before we start gameplay
+        // otherwise we will have the camera fly to where the player is
+        cam.transform.position = playerTransform.position;
+    }
+
+    private Vector3 currentVelocity;
+
+    void Update()
+    {
+        var tmpPos = Vector3.SmoothDamp(cam.transform.position, playerTransform.position, ref currentVelocity, 0.2f);
+        cam.transform.position = new Vector3(tmpPos.x, tmpPos.y, -10);
+    }
+}
