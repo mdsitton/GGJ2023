a002d62d9e795989c5574e01840f249291e235a0
michaelantnewton <michael.ant.newton@gmail.com>
2023-02-04 04:03:21 -0600

    Boy (plant) be shooting ('Vine' prefab goes in direction of mouse when clicked)
    gameplay testing scene
    collision is weird and not working yet though I don't understand it, check it out in vine shoot script

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+
+    public GameObject Vine;
+    public Vector3 targetPos;
+    public Vector3 playerPos;
+    public Rigidbody2D playerBody;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -20,6 +27,12 @@ public class PlayerMove : MonoBehaviour
                 Debug.Log(mousePos.x);
                 Debug.Log(mousePos.y);
             }
+
+            targetPos = Camera.main.ScreenToWorldPoint(mousePos);
+            targetPos.z = 0;
+
+            Instantiate(Vine, playerBody.position, Quaternion.identity);
+
         }
 
     }
--- Assets/Scripts/ShootVine.cs
+++ Assets/Scripts/ShootVine.cs
@@ -4,10 +4,33 @@ using UnityEngine;
 
 public class ShootVine : MonoBehaviour
 {
+
+
+    public float vineSpeed = 0.000002f;
+
+    public Rigidbody2D myRigidBody;
+
+    private Vector2 heading;
+    private Vector2 direction;
+
+
+
+    
+    public Vector3 targetPos;
+    public Vector2 targetPos2D;
+    public Vector2 playerPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       myRigidBody = GetComponent<Rigidbody2D>();
+       playerPos = myRigidBody.position;
+       targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+       targetPos.z = 0;
+       targetPos2D = targetPos;
+       heading = targetPos2D - playerPos;
+       direction = heading / heading.magnitude;
+       myRigidBody.velocity += direction * vineSpeed;
     }
 
     // Update is called once per frame
@@ -15,4 +38,15 @@ public class ShootVine : MonoBehaviour
     {
         
     }
+
+    void OnCollisionEnter(Collision col){
+        if(col.gameObject.name == "Vine"){
+            if (col.gameObject.tag == "Platform"){
+                myRigidBody.velocity = new Vector2(0,0);
+                print("collide");
+            }
+        }
+    }
+
+
 }
