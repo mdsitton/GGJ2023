fc01c3adeb5128b6bd57117a83c3cda583518296
Matthew Sitton <matthewsitton@gmail.com>
2023-02-05 17:21:51 -0600

    Fix scene transition

--- Assets/Scripts/SceneSwitch.cs
+++ Assets/Scripts/SceneSwitch.cs
@@ -7,7 +7,8 @@ using UnityEngine.SceneManagement;
 public class SceneSwitch : MonoBehaviour
 {
 
-	void OnTriggerEnter2D(Collider2D other){
-		SceneManager.LoadScene(2);
-	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene(1);
+    }
 }
