eb850d3af9806a6b3e8a524ad88529a0501b5af3
Olmer "Sylastial <olmercr@gmail.com>
2023-02-07 04:09:39 -0600

    Water Pick Heals 5 HP Prefab
    Can change heal amount in inspector PickUp Item Component
    
    Moved used code to folder

--- Assets/Scripts/PickUpItem.cs
+++ Assets/Scripts/PickUpItem.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class PickUpItem : MonoBehaviour
 {
+    
+    [SerializeField] float healPointsPotionAmount = 5f;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.tag == "Player")
         {
+            var heal = collision.GetComponent<IAttackable>();
+            if (heal != null)
+            {
+                heal. Attack(- healPointsPotionAmount);
+            }
             this.gameObject.SetActive(false);
             Debug.Log("Player Power UP");
         }
