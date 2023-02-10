b0909f593b0499fcd60510d90ca7d350becfa317
Olmer "Penny" Chavez-Ramirez <32857108+PennysBrain@users.noreply.github.com>
2023-02-10 02:30:45 -0600

    Quite Button For game Builds
    Quite button main menu , Worlds 1-3

---
+++ Assets/Scripts/QuiteGame.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuiteGame : MonoBehaviour
+{
+    public void ExitGamePlantyIsNotQuitter()
+    {
+        Application.Quit();
+    }
+}
